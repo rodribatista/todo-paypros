22115f611fd575c830cd64c50109204fb20d1531
Rodrigo <rodribatista@icloud.com>
2025-11-11 15:47:46 -0300

    feat(server): add base nest project

---
+++ server/src/app.controller.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+    }).compile();
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "server is running"', () => {
+      expect(appController.getHealth()).toBe('server is running');
+    });
+  });
+});
