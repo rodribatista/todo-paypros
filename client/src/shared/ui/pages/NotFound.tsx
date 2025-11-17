43637a00e5f420d36844edea51def425732fdfef
Rodrigo <rodribatista@icloud.com>
2025-11-17 10:03:49 -0300

    feat(client): add not found page with go home link

--- client/src/shared/ui/pages/NotFound.tsx
+++ client/src/shared/ui/pages/NotFound.tsx
@@ -1,4 +1,13 @@
+import { Link } from "react-router-dom";
+
 function NotFound() {
-  return <h1>NotFound</h1>;
+  return (
+  <div className="flex flex-col justify-center text-center gap-5">
+    <h1 className="text-4xl">Página no encontrada</h1>
+    <Link to="/" className="text-neutral-400 hover:underline">
+      Volver al inicio
+    </Link>
+  </div>
+  );
 }
 export default NotFound;
